b11b1ca0f4af820598cefa50cc9ee0042e581fe7
Jadyn Gomez <jadyngg19@gmail.com>
2023-05-08 18:31:44 -0400

    Adding in functions to server.js

--- server.js
+++ server.js
@@ -1,14 +1,80 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
+let ceateNoteData = [];
+
 app.use(express.json());
-app.use(express.static("public"));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.static(path.join(__dirname, "public")));
 
-const apiRoutes = require("./Develop/routes/apiRoutes");
-app.use(apiRoutes);
-const htmlRoutes = require("./Develop/routes/htmlRoutes");
-app.use(htmlRoutes);
+app.get("/api/notes", function (err, res) {
+try {
+    createNoteData = fs.readFileSync("db/db.json", "utf8");
+    console.log("Shouting hello from the server!");
+    createNoteData = JSON.parse(createNoteData);
+} catch (err) {
+    console.log("\n error (catch err app.get):");
+    console.log(err);
+}
+res.json(createNoteData);
+});
 
-app.listenerCount(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.post("/api/notes", function (req, res) {
+    try {
+        createNoteData = fs.readFileSync("./db/db.json", "utf8");
+        console.log(ceateNoteData);
+        createNoteData = JSON.parse(createNoteData);
+        req.body.id = createNoteData.length;
+        createNoteData.push(req.body);
+        createNoteData = JSON.stringify(createNoteData);
+        fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) { 
+            if (err) throw err;
+        });
+        res.json(JSON.parse(createNoteData));
+    } catch (err) {
+      throw err;
+      console.error(err);
+    }
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    try {
+        createNoteData = fs.readFileSync("./db/db.json", "utf8");
+        createNoteData = JSON.parse(createNoteData);
+        createNoteData = createNoteData.filter(function (note) {
+            return note.id != req.params.id;
+        });
+
+        fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) { 
+            if (err) throw err;
+        });
+        res.send(JSON.parse(createNoteData));
+    } catch (err) {
+      throw err;
+      console.log(err);
+    }
+});
+
+app.get("/notes", function (req, res) { 
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("*", function (req, res) { 
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/api/notes", function (req, res) { 
+    return res.sendFile(path.json(__dirname, "db/db.json"));
+});
+
+app.listen(PORT, function () {
+    console.log("MY SERVER IS LISTENING: " + PORT);
+});
